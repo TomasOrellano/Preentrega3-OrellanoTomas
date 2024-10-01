bcce0a38597c50cc5ef0034a311bc5ac3d152881
Tomas Orellano <tomiiorellano@gmail.com>
2024-10-01 19:14:47 -0300

    Correccion Link 6 + add script

---
+++ js/mostrarFormulario.js
@@ -0,0 +1,6 @@
+function mostrarFormulario() {
+    // Ocultar los botones
+    document.getElementById('grupoBotones').style.display = 'none';
+    // Mostrar el formulario de registro
+    document.getElementById('formularioRegistro').style.display = 'block';
+}
